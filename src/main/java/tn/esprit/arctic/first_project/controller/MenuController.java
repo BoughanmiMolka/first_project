f31118d9a50f00da5d407bc1f62c0e952a39c8d6
BoughanmiMolka <157849165+BoughanmiMolka@users.noreply.github.com>
2025-03-11 01:10:03 +0100

    Menu & chef cuisinier Controller

--- src/main/java/tn/esprit/arctic/first_project/controller/MenuController.java
+++ src/main/java/tn/esprit/arctic/first_project/controller/MenuController.java
@@ -1,10 +1,15 @@
 package tn.esprit.arctic.first_project.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.arctic.first_project.entities.Menu;
 import tn.esprit.arctic.first_project.entities.TypeMenu;
 import tn.esprit.arctic.first_project.repository.MenuRepo;
+import tn.esprit.arctic.first_project.service.IMenuService;
+import tn.esprit.arctic.first_project.service.MenuService;
+
 
 import tn.esprit.arctic.first_project.service.IMenuService;
 
@@ -16,27 +21,34 @@ import java.util.List;
 
 public class MenuController {
     IMenuService menuService;
+    @Autowired
+    MenuRepo menuRepo;
 
-    @GetMapping("/findMenuByTypeMenuOrderByPrixTotal/{typeMenu}")
-    public List<Menu> findMenuByTypeMenuOrderByPrixTotal (PathVariable("typeMenu") String typeMenu){
-        System.out.println(typeMenu);
-        TypeMenu typeMenu = TypeMenu.valueOf(typeMenu);
-        return menuService.findMenuByTypeMenuOrderByPrixTotal(typeMenu);
+
+    @GetMapping("/retrieve-all-menus")
+    public List<Menu> getMenus(){
+        return menuService.retrieveAllMenus();
     }
     @GetMapping("/retrieve-menu/MenuId")
-    public Menu retrieveMenu(Long menuId){
+    public Menu retrieveMenu(@PathVariable Long menuId){
         return menuService.retrieveMenu(menuId);
     }
     @PostMapping("/add-menu")
-    public Menu addMenu(Menu e) {
+    public Menu addMenu(@RequestBody Menu e) {
         return menuService.addMenu(e);
     }
     @PutMapping("/update-menu")
-    public Menu updateMenu(Menu e) {
+    public Menu updateMenu(@RequestBody Menu e) {
         return menuService.updateMenu(e);
     }
     @DeleteMapping("/remove-menu/{MenuId}")
     public void removeMenu(Long menuId) {
         menuService.removeMenu(menuId);
+    }
+
+    @GetMapping("/getMenuByTypeandOrdre/{typeMenu}")
+    public List<String> findMenuByTypeMenuOrderByPrixTotal(@PathVariable("typeMenu") String typeMenuStr) {
+        TypeMenu typeMenu = TypeMenu.valueOf(typeMenuStr.toUpperCase());
+        return menuRepo.findMenuByTypeMenuOrderByPrixTotal(typeMenu);
     }
 }
